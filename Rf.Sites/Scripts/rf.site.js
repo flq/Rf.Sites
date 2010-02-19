5b8d4ec8bf7b37fe23708ed5acf3bc94a4873747
flq <fquednau@gmail.com>
2010-02-19 23:20:51 +0100

    javascript into own file and added validation to comment posting

---
+++ Rf.Sites/Scripts/rf.site.js
@@ -0,0 +1,21 @@
+﻿$.validator.setDefaults({
+  submitHandler: function() {
+  var f = $("#comment_post");
+  var act = f.attr("action");
+  var ser = f.serialize();
+  $.post(act, ser, function() { $("#after_comment_post").fadeIn("slow"); });
+  return false;
+  }
+});
+
+jQuery(document).ready(function($) {
+  $('a[rel*=box]').facebox({
+    loadingImage: '/files/images/facebox/loading.gif',
+    closeImage: '/files/images/facebox/closelabel.gif'
+  });
+  $('#archive').treeview({ url: "/chronics" });
+  $("#comment_post").validate();
+  jQuery().ajaxStart(function() {
+    $("#comment_post").fadeOut("slow");
+  });
+});
